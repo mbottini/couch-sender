73d590fdf595a12cd3c0a72cc53672998def197f
Mike Bottini <mbottini1@gmail.com>
2017-05-26 15:49:09 -0700

    Change XML parsing to line-by-line script.

--- splitter.py
+++ splitter.py
@@ -1,42 +1,103 @@
 import os
-import xml.etree.ElementTree as ET
+import re
 
-def parseAll():
-    os.makedirs("splitXML", exist_ok=True)
+attrRE = re.compile(r'^.*NAME="(.*)".*$')
 
-    print("Inside root splitter.")
-    tree = ET.parse('dblp-data.xml')
-    root = tree.getroot()
+BATCH_SIZE = 10000
 
-    child = root.find('OBJECTS')
-    print("Splitting OBJECTS")
-    filename = 'splitXML/' + child.tag + ".xml"
-    with open(filename, 'w') as f:
-        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-        f.write('<!DOCTYPE PROX3DB SYSTEM "prox3db.dtd">\n')
-        f.write('<PROX3DB>\n')
-        f.write(ET.tostring(child, encoding="unicode"))
-        f.write('</PROX3DB>')
+def createDocFiles(filename, docType):
+    print("Splitting", docType)
+    prelude = ('<?xml version="1.0" encoding="UTF-8"?>\n' +
+              '<!DOCTYPE PROX3DB SYSTEM "prox3db.dtd">\n' +
+              '<PROX3DB>\n' +
+              '    <{0}>\n'.format(docType))
+    epilogue = '    </{0}>\n</PROX3DB>'.format(docType)
+    with open(filename) as f:
+        fileNumber = 1
+        counter = 0
 
-    child = root.find('LINKS')
-    print("Splitting", child.tag)
-    filename = 'splitXML/' + child.tag + ".xml"
-    with open(filename, 'w') as f:
-        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-        f.write('<!DOCTYPE PROX3DB SYSTEM "prox3db.dtd">\n')
-        f.write('<PROX3DB>\n')
-        f.write(ET.tostring(child, encoding="unicode"))
-        f.write('</PROX3DB>')
+        # Get to the OBJECTS portion of the file.
+        currentLine = f.readline()
+        while currentLine != '    <{0}>\n'.format(docType):
+            currentLine = f.readline()
+        currentLine = f.readline()
 
-    child = root.find('ATTRIBUTES')
-    for attribute in child:
-        print("Splitting", attribute.attrib['NAME'])
-        filename = 'splitXML/' + attribute.attrib['NAME'] + '.xml'
-        with open(filename, 'w') as f:
-            f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-            f.write('<!DOCTYPE PROX3DB SYSTEM "prox3db.dtd">\n')
-            f.write('<PROX3DB>\n')
-            f.write(ET.tostring(attribute, encoding="unicode"))
-            f.write('</PROX3DB>')
+        #Now, we write BATCH_SIZE objects to each file.
 
-parseAll()
+        print("At 0.")
+        w = open('splitXML/' + docType + str(fileNumber) + '.xml', 'w')
+        w.write(prelude)
+        currentFile = ""
+        while currentLine != '    </{0}>\n'.format(docType):
+            if counter == BATCH_SIZE:
+                print("At", counter * fileNumber)
+                w.write(currentFile)
+                w.write(epilogue)
+                w.close()
+                fileNumber += 1
+                w = open('splitXML/' + docType + str(fileNumber) + '.xml', 'w')
+                w.write(prelude)
+                currentFile = ""
+                counter = 0
+            currentFile += currentLine
+            currentLine = f.readline()
+            counter += 1
+        w.write(currentFile)
+        w.write(epilogue)
+        w.close()
+
+def createAttributeFiles(filename):
+    prelude = ('<?xml version="1.0" encoding="UTF-8"?>\n' +
+              '<!DOCTYPE PROX3DB SYSTEM "prox3db.dtd">\n' +
+              '<PROX3DB>\n' +
+              '    <ATTRIBUTES>\n')
+    epilogue = '        </ATTRIBUTE>\n    </ATTRIBUTES>\n</PROX3DB>'
+
+    with open(filename) as f:
+        fileNumber = 1
+        counter = 0
+
+        # Get to the ATTRIBUTES portion of the file.
+        currentLine = f.readline()
+        while currentLine != '    <ATTRIBUTES>\n':
+            currentLine = f.readline()
+        currentLine = f.readline()
+
+        fileNumber = 1
+        while currentLine and currentLine != '    </ATTRIBUTES>\n':
+            attrName = re.match(r'^.*NAME=\"(.*?)\".*$', currentLine).group(1)
+            print("Splitting", attrName)
+            newPrelude = prelude + currentLine
+            currentLine = f.readline()
+            counter = 0
+            fileNumber = 1
+            w = open('splitXML/' + attrName + str(fileNumber) + '.xml', 'w')
+            w.write(newPrelude)
+            currentFile = ""
+            while currentLine and currentLine != '        </ATTRIBUTE>\n':
+                currentFile += currentLine
+                currentLine = f.readline()
+                currentFile += currentLine
+                currentLine = f.readline()
+                currentFile += currentLine
+                currentLine = f.readline()
+                counter += 1
+                if counter == BATCH_SIZE:
+                    print("At", counter * fileNumber)
+                    w.write(currentFile)
+                    w.write(epilogue)
+                    w.close()
+                    fileNumber += 1
+                    w = open('splitXML/' + 'ATTRIBUTE' + str(fileNumber) + '.xml', 'w')
+                    w.write(newPrelude)
+                    currentFile = ""
+                    counter = 0
+            w.write(currentFile)
+            w.write(epilogue)
+            w.close()
+            currentLine = f.readline()
+
+createDocFiles('dblp-data.xml', 'OBJECTS')
+createDocFiles('dblp-data.xml', 'LINKS')
+createAttributeFiles('dblp-data.xml')
+
